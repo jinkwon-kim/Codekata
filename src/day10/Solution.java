aadbd5047af57a664e6930d063ac626e912c171b
김진권(Spring_7기) <sjh99021800@gmail.com>
2025-05-16 09:20:57 +0900

    day10 배열의 평균 구현 완료

---
+++ src/day10/Solution.java
@@ -0,0 +1,13 @@
+package day10;
+
+public class Solution {
+    public double solution(int[] numbers) {
+        double answer = 0;
+        double sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        answer = sum/numbers.length;
+        return answer;
+    }
+}
