8ab052c040840231e1f81caedfa56cb265f35120
김진권(Spring_7기) <sjh99021800@gmail.com>
2025-05-19 09:14:57 +0900

    day11 짝수와 홀수 구현 완료

---
+++ src/day11/solution.java
@@ -0,0 +1,10 @@
+package day11;
+
+public class solution {
+    public String solution(int num) {
+        String answer = "";
+        if (num%2 == 0) {answer = "Even";}
+        else {answer = "Odd";}
+        return answer;
+    }
+}
