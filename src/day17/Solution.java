674944e5712cd67394458b536dbd11c4af06e275
김진권(Spring_7기) <sjh99021800@gmail.com>
2025-05-28 09:27:38 +0900

    day17 자연수 뒤집어 배열로 만들기 구현 완료

---
+++ src/day17/Solution.java
@@ -0,0 +1,15 @@
+package day17;
+
+public class Solution {
+    public int[] solution(long n) {
+        String[] numArr = String.valueOf(n).split("");
+        int[] answer = new int[numArr.length];
+
+        for (int i = 0; i < numArr.length; i++) {
+            String num = numArr[numArr.length - i - 1];
+            answer[i] = Integer.parseInt(num);
+        }
+
+        return answer;
+    }
+}
