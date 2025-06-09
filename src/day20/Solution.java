cf401899536799c37ceb8fab5722f3e7c9e9c7de
김진권(Spring_7기) <sjh99021800@gmail.com>
2025-06-09 09:31:39 +0900

    day20 정수 내림차순으로 배치하기 구현 완료

---
+++ src/day20/Solution.java
@@ -0,0 +1,28 @@
+package day20;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Solution {
+    public long solution(long n) {
+        String str = ""+n;
+        int[] arr = new int[str.length()];
+        for(int i=0; i< str.length(); i++) {
+            arr[i] = (int)(n%10);
+            n/=10;
+        }
+
+        Integer[] tmp = Arrays.stream(arr).boxed().toArray(Integer[]::new);
+        Arrays.sort(tmp, Collections.reverseOrder());
+
+        long answer = 0;
+        int num = tmp.length;
+
+        for(int i=0; i< tmp.length; i++) {
+            answer += tmp[i] * (long) Math.pow(10, num-1);
+            num-=1;
+
+        }
+        return answer;
+    }
+}
